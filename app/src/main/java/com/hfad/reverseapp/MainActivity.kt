4f49b31aa279e974159b6fe6838cd7997db9a70f
staswd <staskarpyuk08@gmail.com>
2023-05-20 14:39:09 +0300

    git config --global user.name "Your Name"

--- app/src/main/java/com/hfad/reverseapp/MainActivity.kt
+++ app/src/main/java/com/hfad/reverseapp/MainActivity.kt
@@ -9,7 +9,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val navHostFragment=supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        val navController =navHostFragment.navController
+//        val navHostFragment=supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+//        val navController =navHostFragment.navController
+
     }
 }
