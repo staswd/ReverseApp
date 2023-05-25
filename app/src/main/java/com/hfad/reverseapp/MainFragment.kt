10365bb558e46f0674dee56d0d0ddea2d1615e60
staswd <staskarpyuk08@gmail.com>
2023-05-25 21:26:14 +0300

    git config --global user.name "Your Name"

--- app/src/main/java/com/hfad/reverseapp/MainFragment.kt
+++ app/src/main/java/com/hfad/reverseapp/MainFragment.kt
@@ -1,15 +1,13 @@
 package com.hfad.reverseapp
 
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.get
-import androidx.lifecycle.viewmodel.viewModelFactory
-import androidx.navigation.findNavController
 import com.hfad.reverseapp.databinding.FragmentMainBinding
 
 
@@ -32,15 +30,49 @@ class MainFragment : Fragment() {
         var filter=""
         viewModel=ViewModelProvider(this).get(MainFragmentViewModel::class.java)
 
+        binding.textToReverse.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable) {}
+            override fun beforeTextChanged(
+                s: CharSequence, start: Int,
+                count: Int, after: Int
+            ) {
+            }
 
-        binding.reverseButton.setOnClickListener(){
-
-            textToReverse=binding.textToReverse.text.toString()
+            override fun onTextChanged(
+                s: CharSequence, start: Int,
+                before: Int, count: Int
+            ) {
+                textToReverse=binding.textToReverse.text.toString()
             filter=binding.filter.text.toString()
-            val reversedText=viewModel.reverseText(textToReverse,filter)
-            binding.reversed.setText(reversedText)
+                binding.reversed.text=viewModel.reverseText(textToReverse,filter)
+            }
+        })
+        binding.filter.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable) {}
+            override fun beforeTextChanged(
+                s: CharSequence, start: Int,
+                count: Int, after: Int
+            ) {
+            }
 
-        }
+            override fun onTextChanged(
+                s: CharSequence, start: Int,
+                before: Int, count: Int
+            ) {
+                textToReverse=binding.textToReverse.text.toString()
+                filter=binding.filter.text.toString()
+                binding.reversed.text=viewModel.reverseText(textToReverse,filter)
+            }
+        })
+
+//        binding.reverseButton.setOnClickListener(){
+//
+//            textToReverse=binding.textToReverse.text.toString()
+//            filter=binding.filter.text.toString()
+//            val reversedText=viewModel.reverseText(textToReverse,filter)
+//            binding.reversed.setText(reversedText)
+//
+//        }
 
 
         return view
--- app/src/main/java/com/hfad/reverseapp/MainFragmentViewModel.kt
+++ app/src/main/java/com/hfad/reverseapp/MainFragmentViewModel.kt
@@ -3,37 +3,58 @@ package com.hfad.reverseapp
 import androidx.lifecycle.ViewModel
 
 class MainFragmentViewModel():ViewModel() {
-
-
-
-
-
     fun reverseText(textToReverse:String,filter:String):String{
      val splittedText=textToReverse.split(" ")
-        val reversedText = splittedText.map{ reverseWord(it,filter)}
-
+        var reversedText: List<String>
+        if(filter==""){
+            reversedText= splittedText.map{ reverseWord(it)}
+        }else{
+            reversedText= splittedText.map{ reverseWordWithFilter(it,filter)}
+        }
         val result=reversedText.joinToString(" ")
-
         return result
-
-
-
-
     }
-
-
-    fun reverseWord( word:String, filter: String): String{
-        val filterList=filter.split("")
-        var resultWord:String=""
-        word.split("").mapIndexed { index, it->
-            if (filterList.contains(it)) {
-                resultWord+=it
-            }
-            else{
-                resultWord+=word[word.length-(index)]
+    fun reverseWord(word:String):String{
+        val indexes= mutableListOf<Int>()
+        val symbols= mutableListOf<Char>()
+        val resultWord= word.toCharArray().toMutableList()
+        resultWord.forEachIndexed{index, it->
+            if(it.code !in 97..122&&it.code !in 65..90){
+                indexes.add(index)
+                symbols.add(it)
             }
         }
-        return resultWord
+        if(indexes.size>0) {
+            indexes.reversed().forEach { resultWord.removeAt(it) }
+        }
+        resultWord.reverse()
+        if(indexes.size>0) {
+            for((j, i) in indexes.withIndex()){
+                resultWord.add(i, symbols[j])
+            }
+        }
+                return resultWord.joinToString ( "" )
     }
-
+    fun reverseWordWithFilter( word:String, filter: String): String{
+        val indexes= mutableListOf<Int>()
+        val symbols= mutableListOf<Char>()
+        val filterList=filter.toCharArray().toMutableList()
+        var resultWord=word.toCharArray().toMutableList()
+        resultWord.mapIndexed { index, it->
+            if (filterList.contains(it)) {
+                indexes.add(index)
+                symbols.add(it)
+            }
+        }
+        if(indexes.size>0) {
+            indexes.reversed().forEach { resultWord.removeAt(it) }
+        }
+        resultWord.reverse()
+        if(indexes.size>0) {
+            for((j, i) in indexes.withIndex()){
+                resultWord.add(i, symbols[j])
+            }
+        }
+        return resultWord.joinToString ( "" )
+    }
 }
