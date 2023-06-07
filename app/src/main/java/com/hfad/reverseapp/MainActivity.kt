5bde2ec91075c0d9c88121403da56cd6a124de15
staswd <staskarpyuk08@gmail.com>
2023-06-07 18:57:51 +0300

    user interface according to the terms of reference

--- app/src/main/java/com/hfad/reverseapp/MainActivity.kt
+++ app/src/main/java/com/hfad/reverseapp/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.hfad.reverseapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.R.attr.radius
 import android.os.Bundle
-import androidx.navigation.fragment.NavHostFragment
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.shape.CornerFamily
+import com.google.android.material.shape.MaterialShapeDrawable
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val toolbar = findViewById<MaterialToolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+//        val radius = getResources().getDimension(R.dimen.radius).toFloat()
+        val toolbarBackground = toolbar.background as MaterialShapeDrawable
+        toolbarBackground.shapeAppearanceModel = toolbarBackground.shapeAppearanceModel.toBuilder()
+            .setBottomRightCorner(CornerFamily.ROUNDED, 50f)
+            .setBottomLeftCorner(CornerFamily.ROUNDED, 50f).build()
     }
 }
--- app/src/main/java/com/hfad/reverseapp/MainFragment.kt
+++ app/src/main/java/com/hfad/reverseapp/MainFragment.kt
@@ -1,44 +1,66 @@
 package com.hfad.reverseapp
 
+
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.hfad.reverseapp.databinding.FragmentMainBinding
+
 class MainFragment : Fragment() {
     private var _binding: FragmentMainBinding? = null
     private val binding get() = _binding!!
     lateinit var viewModel: MainFragmentViewModel
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        _binding = FragmentMainBinding
-            .inflate(inflater, container, false)
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentMainBinding.inflate(inflater, container, false)
         val view = binding.root
         viewModel = ViewModelProvider(this).get(MainFragmentViewModel::class.java)
-        binding.textToReverse.addTextChangedListener(Watcher(binding.textToReverse,binding.filter,binding.reversed))
-        binding.filter.addTextChangedListener(Watcher(binding.textToReverse,binding.filter,binding.reversed))
-//        var textToReverse = ""
-//        var filter = ""
-//        class Watcher : TextWatcher {
-//            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
-//            override fun onTextChanged(
-//                s: CharSequence, start: Int,
-//                before: Int, count: Int
-//            ) {
-//            }
-//
-//            override fun afterTextChanged(s: Editable?) {
-//                textToReverse = binding.textToReverse.text.toString()
-//                filter = binding.filter.text.toString()
-//                binding.reversed.text = viewModel.reverseText(textToReverse, filter)
-//            }
-//        }
-//        binding.textToReverse.addTextChangedListener(Watcher())
-//        binding.filter.addTextChangedListener(Watcher())
+        binding.textToReverse.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) =
+                if (binding.textToReverse.text.toString() != "") {
+                    binding.reverseButton.visibility = View.VISIBLE
+//                    binding.reverseButton.background =
+//                        getResources().getDrawable(R.drawable.button_bg)
+                } else {
+                    binding.reverseButton.visibility = View.GONE
+                }
+            override fun afterTextChanged(s: Editable?) {}
+        })
+//        binding.filter.addTextChangedListener(
+//            Watcher(
+//                binding.textToReverse,
+//                binding.filter,
+//                binding.anagram
+//            )
+//        )
+
+        binding.textToReverse.setOnFocusChangeListener { v, hasFocus ->
+            if (hasFocus) {
+                binding.textHint.visibility = View.VISIBLE
+                binding.textToReverse.hint = ""
+            } else {
+                binding.textHint.visibility = View.GONE
+                binding.textToReverse.hint = "Enter text for anagram"
+            }
+        }
+
+        binding.reverseButton.setOnClickListener() {
+            val textToReverse = binding.textToReverse.text.toString()
+            val filter = binding.filter.text.toString()
+            val reversedText = viewModel.reverseText(textToReverse, filter)
+            binding.anagram.setText(reversedText)
+            binding.anagram.textSize = 24f
+            binding.anagram.setTextColor(getResources().getColor(R.color.my_light_primary))
+        }
+
         return view
     }
 }
